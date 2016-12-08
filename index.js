97603ddc93f3eced1cc3c3661bc8d7b4d64c684c
Dan Williams <deedubs@Dans-MacBook-Air.local>
2016-12-08 09:54:44 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import {add} from './process';
+
+let result :number = add(1,2);
+
+let resultAsString :string = add(1,2);
+
+console.log(result, resultAsString)
+
+let soom = "boom"
---
+++ process.js
@@ -0,0 +1,6 @@
+// @flow
+
+export function add(number: number, number2: number): number{
+
+    return number + number2;
+}
